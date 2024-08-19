1e931a23fe1c865cf653fb2e7c35759a3d79a610
spmeikon <pgeneta@gmail.com>
2024-08-19 11:36:31 +0100

    library set up

---
+++ scripts/1_libraries.R
@@ -0,0 +1,27 @@
+if (!require(tidyverse))
+    install.packages("tidyverse", dependencies = T)
+library(tidyverse)
+
+if (!require(sf))
+    install.packages("sf", dependencies = T)
+library(sf)
+
+if (!require(leaflet))
+    install.packages("leaflet", dependencies = T)
+library(leaflet)
+
+if (!require(ggplot2))
+    install.packages("ggplot2", dependencies = T)
+library(ggplot2)
+
+if (!require(scales))
+    install.packages("scales", dependencies = T)
+library(scales)
+
+if (!require(shiny))
+    install.packages("shiny", dependencies = T)
+library(shiny)
+
+if (!require(janitor))
+    install.packages("janitor", dependencies = T)
+library(janitor)
